a16e24cf67795d0a8f03859c06c091e7201b1722
grenos <vasilis.green@gmail.com>
2024-02-11 15:30:12 +0100

    fix: check andoid cam perms are ok

--- packages/frontend/src/Components/Body.tsx
+++ packages/frontend/src/Components/Body.tsx
@@ -1,5 +1,5 @@
-import { Flex, useDisclosure } from "@chakra-ui/react";
-import { useCallback, useState } from "react";
+import { Flex, useDisclosure, useToast } from "@chakra-ui/react";
+import { useCallback, useEffect, useState } from "react";
 import { SubmitDrawer } from "./SubmitDrawer";
 import { CameraButton } from "./CameraButton";
 import { FileType } from "../Models";
@@ -23,9 +23,51 @@ export const Body = () => {
         [onSubmitOpen]
     );
 
+    const [perms, setPerms] = useState<string[]>([]);
+
+    const toast = useToast();
+
+    useEffect(() => {
+        async function init() {
+            try {
+                const devices = await navigator.mediaDevices.enumerateDevices();
+
+                // filter on video inputs, and map to query object
+                const queries = devices
+                    .filter(({ kind }) => kind === "videoinput")
+                    .map(({ deviceId }) => ({ name: "camera", deviceId }));
+
+                const promises = queries.map((queryObj) =>
+                    navigator.permissions.query(
+                        queryObj as PermissionDescriptor
+                    )
+                );
+
+                const results = await Promise.all(promises);
+                // log the state of each camera
+                results.forEach(({ state }, i) =>
+                    setPerms((prev) => [...prev, `Camera ${i}: ${state}`])
+                );
+            } catch (error) {
+                toast({
+                    title: "Error getting camera permissions.",
+                    description:
+                        "Please enable camera access in your phone settings for VeWWorld.",
+                    status: "error",
+                    duration: 6000,
+                    isClosable: true,
+                });
+            }
+        }
+
+        init();
+    }, [toast]);
+
     return (
         <Flex p={6} justify="center" alignItems="center" direction="column">
             <CameraButton onFotoTaken={onFotoTaken} />
+
+            {perms && perms.map((perm, i) => <p key={i}>{perm}</p>)}
 
             <SubmitDrawer
                 isOpen={isSubmitOpen}
